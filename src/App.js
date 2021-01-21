be8a493b4ac8fc45c820147c278001f2bc6774bb
Tabatha Slatton <valbatha@gmail.com>
2021-01-21 08:22:34 -0500

    created a button to increase count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import {useState} from "react"
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hiiii</h1>
+        {/* <button onClick={() => setCount( count + 1)}>Increase Count</button> */}
+        <button onClick={handleClick}>Increase Count</button>
+        <p>{count}</p>
     </div>
-  );
+    );
 }
-
+ 
 export default App;
