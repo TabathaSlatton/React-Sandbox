34f0e7bb7a564b59e2a610c47b5918081b7102e0
Tabatha Slatton <valbatha@gmail.com>
2021-01-21 10:02:38 -0500

    used React Hooks to fetch and display data from an external api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+// https://randomuser.me/api
 
-function App() {
+const App = () => {
+  const [ userDataString, setUserDataString ] = useState('')
+  const [ usersData, setUsersData] = useState([])
+  const [ page, setPage] = useState(1)
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api?page=${page}`)
+      .then(resp => resp.json())
+      .then((data) => {
+        setUserDataString(JSON.stringify(data.results[0]))
+        console.log(data.results[0].name.first)
+        setUsersData([...usersData, data.results[0]])
+      })
+  }, [page]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log('users', usersData)}
+      <h1>Welcome to the sandpit</h1>
+      {/* <p>{userDataString}</p> */}
+      {usersData.map(user => (
+        <div key={user.id.value}>
+          <h4>{user.name.first} {user.name.last}</h4>
+          <img src={user.picture.large}></img>
+        </div>
+      )
+      )}
+      <button onClick={() => setPage(page + 1)}>Next User</button>
     </div>
-  );
+  )
 }
 
 export default App;
